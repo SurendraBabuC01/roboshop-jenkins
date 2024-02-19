80a52227cf91d8b97c17141316f9f44bc0beae8a
surendra Babu C <surendra9703802865@gmail.com>
2024-02-19 19:20:45 +0530

    Jenkins

--- vars/python.groovy
+++ vars/python.groovy
@@ -13,25 +13,26 @@ def call() {
 
         stages {
 
-            stage('code quality') {
+            stage('Code Quality') {
                 steps {
-                    sh 'echo Code Quality'
+                    sh 'ls -l'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.46.48:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
 
-            stage('unit test cases') {
+            stage('Unit Test Cases') {
                 steps {
                     sh 'echo Unit Test Cases'
                 }
             }
 
-            stage('checkmarx SAST scan') {
+            stage('Checkmarx SAST Scan') {
                 steps {
                     sh 'echo Checkmarx SAST Scan'
                 }
             }
 
-            stage('checkmarx SCA scan') {
+            stage('Checkmarx SCA Scan') {
                 steps {
                     sh 'echo Checkmarx SCA Scan'
                 }
